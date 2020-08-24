26521fb301ed5158d13d5d3f8c4211d6fdd9e16d
Dean Wyatte <dwyatte@squareup.com>
2020-08-24 11:02:44 -0600

    multimodal example

--- java/src/main/java/SavedModelPredictor.java
+++ java/src/main/java/SavedModelPredictor.java
@@ -3,19 +3,25 @@ import org.tensorflow.Tensor;
 
 public class SavedModelPredictor {
 
-    public static final String savedModelPath = "/Users/dwyatte/GitHub/tensorflow-java-experiments/python/export/use/0";
+    public static final String savedModelPath = "/Users/dwyatte/GitHub/tensorflow-java-experiments/python/export/multimodal/0";
     public static final String tagSet = "serve";
-    public static final String inputName = "serving_default_input_1";
+    public static final String floatInputName = "serving_default_float_input";
+    public static final String stringInputName = "serving_default_string_input";
     public static final String outputName = "StatefulPartitionedCall_1";
 
     public static void main(String[] args) throws Exception {
         SavedModelBundle savedModel = SavedModelBundle.load(savedModelPath, tagSet);
-        // Tensor inputTensor = Tensor.create(0.0f);
-        Tensor inputTensor = Tensor.create(new byte[][] {
+        Tensor floatInputTensor = Tensor.create(new float[][] {
+            {0.0f}, {1.0f}
+        });
+        Tensor stringInputTensor = Tensor.create(new byte[][] {
             "a sentence".getBytes(),
             "b sentence".getBytes(),
         });
-        Tensor outputTensor = savedModel.session().runner().feed(inputName, inputTensor).fetch(outputName).run().get(0);
+        Tensor outputTensor = savedModel.session().runner()
+                                .feed(floatInputName, floatInputTensor)
+                                .feed(stringInputName, stringInputTensor)
+                                .fetch(outputName).run().get(0);
         float[][] output = new float[2][1];
         outputTensor.copyTo(output);
         System.out.println(output[0][0]);
--- python/create_models.py
+++ python/create_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
@@ -20,6 +21,18 @@ class USEModel(tf.keras.Model):
         return self.dense(self.embedding(tf.squeeze(inputs)))
 
 
+class MultimodalModel(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.embedding = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+        self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
+
+    def call(self, inputs):
+        embedding = self.embedding(tf.squeeze(inputs['string_input']))
+        features = tf.concat((inputs['float_input'], embedding), axis=1)
+        return self.dense(features)
+
+
 identity_model = IdentityModel()
 identity_model.compile(loss='binary_crossentropy', optimizer='sgd')
 identity_model.fit([0.0, 1.0], [0.0, 1.0])
@@ -29,3 +42,12 @@ use_model = USEModel()
 use_model.compile(loss='binary_crossentropy', optimizer='sgd')
 use_model.fit(['a sentence', 'b sentence'], [0.0, 1.0])
 use_model.save('export/use/0')
+
+multimodal_inputs = {
+    'string_input': tf.constant(np.array(['a sentence', 'b sentence'])),
+    'float_input': tf.constant(np.array([0.0, 1.0]))
+}
+multimodal_model = MultimodalModel()
+multimodal_model.compile(loss='binary_crossentropy', optimizer='sgd')
+multimodal_model.fit(multimodal_inputs, np.array([0.0, 1.0]))
+multimodal_model.save('export/multimodal/0')
